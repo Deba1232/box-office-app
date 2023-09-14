3ad6df33e0c0899ee6e98ee76d98a7affe286bc1
DeV <debanjan.das90@gmail.com>
2023-09-14 23:57:29 +0530

    fetch shows for Starred page

--- src/api/tvMaze.js
+++ src/api/tvMaze.js
@@ -1,9 +1,9 @@
 const BASE_URL = "https://api.tvmaze.com";
 
-const apiData = async(queryString) => {
-    const response = await fetch(`${BASE_URL}${queryString}`); 
-    return response.json();
-}
+const apiData = async (queryString) => {
+  const response = await fetch(`${BASE_URL}${queryString}`);
+  return response.json();
+};
 
 export const searchShow = (query) => apiData(`/search/shows?q=${query}`);
 
@@ -11,3 +11,9 @@ export const searchActor = (query) => apiData(`/search/people?q=${query}`);
 
 export const getShowById = (showId) =>
   apiData(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getMultipleShowsById = async (showIdArray) => {
+  const promises = showIdArray.map((id) => apiData(`/shows/${id}`));
+
+  return await Promise.all(promises);
+};
--- src/routes/Starred.jsx
+++ src/routes/Starred.jsx
@@ -1,9 +1,34 @@
+import { useQuery } from "react-query";
+
 import { useStarredShows } from "../lib/useStarredShows";
+import { getMultipleShowsById } from "../api/tvMaze";
+import ShowList from "../components/shows/ShowList";
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
 
-  return <div>Starred page, starred shows: {starredShows.length}</div>;
+  const { data: starredShows, error: apiDataError } = useQuery({
+    queryKey: ["starred", starredShowsIds],
+    queryFn: () =>
+      getMultipleShowsById(starredShowsIds).then((result) =>
+        result.map((show) => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+
+  if (apiDataError) {
+    return <div>Error occured: {apiDataError.message}</div>;
+  }
+
+  if (starredShows?.length === 0) {
+    return <div> No shows were starred </div>;
+  }
+
+  if (starredShows?.length > 0) {
+    return <ShowList shows={starredShows} />;
+  }
+
+  return <div>Loading...</div>;
 };
 
 export default Starred;
