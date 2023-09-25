54aa4368d8734eb815bfd42ac14e15c497e58708
DeV <debanjan.das90@gmail.com>
2023-09-25 20:51:40 +0530

    style navbar, radio buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,27 @@ import Show from "./routes/Show";
 
 import MainLayout from "./components/MainLayout";
 
+import { GlobalTheme } from "./theme";
+
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/starred" element={<Starred />}></Route>
-          </Route>
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/starred" element={<Starred />}></Route>
+            </Route>
 
-          <Route path="/show/:showId" element={<Show />} />
+            <Route path="/show/:showId" element={<Show />} />
 
-          <Route path="*" element={<div>not found</div>} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<div>not found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 import { useSearchStr } from "../lib/useSearchStr";
 
@@ -28,7 +29,7 @@ const SearchForm = ({ onSearch }) => {
     <form onSubmit={onFormSubmit}>
       <input type="text" value={searchStr} onChange={onSearchInput} />
 
-      <label>
+      <StyledRadio>
         <input
           type="radio"
           name="search-options"
@@ -36,10 +37,11 @@ const SearchForm = ({ onSearch }) => {
           checked={searchOption === "shows"}
           onChange={onSearchOptionChange}
         />
+        <span />
         Shows
-      </label>
+      </StyledRadio>
 
-      <label>
+      <StyledRadio>
         <input
           type="radio"
           name="search-options"
@@ -47,8 +49,9 @@ const SearchForm = ({ onSearch }) => {
           checked={searchOption === "actors"}
           onChange={onSearchOptionChange}
         />
+        <span />
         Actors
-      </label>
+      </StyledRadio>
 
       <button type="submit" onClick={onSearch}>
         Search
@@ -58,3 +61,49 @@ const SearchForm = ({ onSearch }) => {
 };
 
 export default SearchForm;
+
+const StyledRadio = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 25px;
+  cursor: pointer;
+  font-size: 13px;
+  user-select: none;
+  font-weight: 700;
+  line-height: 1.65;
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+  }
+  span {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 16px;
+    width: 16px;
+    background-color: #fff;
+    border: 2px solid ${({ theme }) => theme.mainColors.blue};
+    border-radius: 50%;
+  }
+  input:checked ~ span {
+    background-color: #fff;
+    border: 2px solid ${({ theme }) => theme.mainColors.blue};
+  }
+  span:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+  input:checked ~ span:after {
+    display: block;
+  }
+  span:after {
+    top: 4px;
+    left: 4px;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.mainColors.blue};
+  }
+`;
